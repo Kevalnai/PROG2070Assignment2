f17200a5b292a586d681fea0fef03d3b4c28c36e
Keval Nai <kevalnai1504@gmail.com>
2025-02-03 23:30:00 -0500

    Created  product class

--- Product.cs
+++ Product.cs
@@ -8,9 +8,35 @@ namespace PROG2070Assignment2
 {
     public class Product
     {
-        public Product()
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int StockAmount { get; set; }
+
+        public Product(int prodID, string prodName, decimal itemPrice, int stockAmount)
         {
-            
+            ProdId = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+
+        }
+
+        public void increaseStock(int amount)
+        {
+            if (amount > 0)
+            {
+                StockAmount += amount;
+            }
+
+
+        }
+        public void DecreaseStock(int amount)
+        {
+            if (amount > 0 && amount <= StockAmount)
+            {
+                StockAmount -= amount;
+            }
         }
     }
 }
