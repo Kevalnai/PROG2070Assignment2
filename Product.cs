ba44f15dbbad5b001fc56aef5dec2f987159b740
Kuolung Cheng <KCheng9482@conestogac.on.ca>
2025-02-04 19:48:11 -0500

    Implement Product class

--- Product.cs
+++ Product.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PROG2070Assignment2
+﻿namespace PROG2070Assignment2
 {
     public class Product
     {
-        
+        private int prodId;
+        private string prodName;
+        private decimal itemPrice;
+        private int stockAmount;
+
+        public int ProdId { get => prodId; set => prodId = value; }
+        public string ProdName { get => prodName; set => prodName = value; }
+        public decimal ItemPrice { get => itemPrice; set => itemPrice = value; }
+        public int StockAmount { get => stockAmount; set => stockAmount = value; }
+
+        public Product(int prodId, string prodName, decimal itemPrice, int stockAmount)
+        {
+            this.prodId = prodId;
+            this.prodName = prodName;
+            this.itemPrice = itemPrice;
+            this.stockAmount = stockAmount;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            this.StockAmount += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            this.stockAmount -= amount;
+        }
     }
 }
