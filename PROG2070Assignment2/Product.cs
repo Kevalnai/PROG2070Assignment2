4bc2f00b3d3d6557ed5189479480ac0507d2407f
Kuolung Cheng <KCheng9482@conestogac.on.ca>
2025-02-04 23:26:24 -0500

    Implement ProductTests class and add 6 test cases

---
+++ PROG2070Assignment2/Product.cs
@@ -0,0 +1,118 @@
+﻿namespace PROG2070Assignment2
+{
+    public class Product
+    {
+        private int prodId;
+        private string prodName;
+        private double itemPrice;
+        private int stockAmount;
+
+        /*
+         * maximum and minimum values:
+         * Product ID: 5 - 50000
+         * Price: $5 - $5000
+         * Stock: 5 - 500000
+         */
+        public int ProdId
+        {
+            get { return prodId; }
+
+            set
+            {
+                if (value < 5 || value > 50000)
+                {
+                    throw new ArgumentException("Product ID must be between 5 and 50000");
+                }
+
+                prodId = value;
+            }
+        }
+
+        public string ProdName
+        {
+            get { return prodName; }
+
+            set { prodName = value; }
+        }
+
+        public double ItemPrice
+        {
+            get { return itemPrice; }
+
+            set
+            {
+                if (itemPrice < 5 || itemPrice > 5000)
+                {
+                    throw new ArgumentException("Item price must be between 5 and 5000");
+                }
+
+                itemPrice = value;
+            }
+        }
+        public int StockAmount
+        {
+            get { return stockAmount; }
+
+            set
+            {
+                if (value < 5 || value > 500000)
+                {
+                    throw new ArgumentException("Stock amount must be between 5 and 500000");
+                }
+
+                stockAmount = value;
+            }
+        }
+
+        public Product()
+        {
+            prodId = 1000;
+            prodName = "TestProduct";
+            itemPrice = 1000.00;
+            stockAmount = 1000;
+        }
+
+        public Product(int prodId, string prodName, double itemPrice, int stockAmount)
+        {
+            if (prodId < 5 || prodId > 50000)
+            {
+                throw new ArgumentException("Product ID must be between 5 and 50000");
+            }
+
+            if (itemPrice < 5 || itemPrice > 5000)
+            {
+                throw new ArgumentException("Item price must be between 5 and 5000");
+            }
+
+            if (stockAmount < 5 || stockAmount > 500000)
+            {
+                throw new ArgumentException("Stock amount must be between 5 and 500000");
+            }
+
+            this.prodId = prodId;
+            this.prodName = prodName;
+            this.itemPrice = itemPrice;
+            this.stockAmount = stockAmount;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            if (StockAmount + amount > 500000)
+            {
+                throw new ArgumentException("Stock amount must be between 5 and 500000");
+            }
+
+            StockAmount += amount;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (StockAmount - amount < 5)
+            {
+                throw new ArgumentException("Stock amount must be between 5 and 500000");
+            }
+
+            StockAmount -= amount;
+        }
+    }
+}
---
+++ PROG2070Assignment2UnitTests/ProductTests.cs
@@ -0,0 +1,133 @@
+using PROG2070Assignment2;
+
+namespace PROG2070Assignment2UnitTests
+{
+    public class ProductTests
+    {
+        private Product testProduct;
+
+        [SetUp]
+        public void Setup()
+        {
+            testProduct = new Product();
+        }
+
+        /* 
+         * Verify that a valid product ID (123) can be set successfully
+         * - Kuolung Cheng
+         */
+        [Test]
+        public void ProdId_Input123_Valid()
+        {
+            // Arrange
+            int input = 123;
+            int expected = 123;
+
+            // Act
+            testProduct.ProdId = input;
+            int actual = testProduct.ProdId;
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        /* 
+         * Verify that setting a product ID (4) below the minimum value (5) will throw an exception
+         * - Kuolung Cheng
+         */
+        [Test]
+        public void ProdId_Input4_Invalid()
+        {
+            // Arrange
+            int input = 4;
+            string expected = "Product ID must be between 5 and 50000";
+
+            // Act
+            var actual = Assert.Throws<ArgumentException>(() => testProduct.ProdId = input);
+
+            // Assert
+            Assert.That(actual.Message, Is.EqualTo(expected));
+        }
+
+        /* 
+         * Verify that setting a product ID (50001) above the minimum value (50000) will throw an exception
+         * - Kuolung Cheng
+         */
+        [Test]
+        public void ProdId_Input50001_Invalid()
+        {
+            // Arrange
+            int input = 50001;
+            string expected = "Product ID must be between 5 and 50000";
+
+            // Act
+            var actual = Assert.Throws<ArgumentException>(() => testProduct.ProdId = input);
+
+            // Assert
+            Assert.That(actual.Message, Is.EqualTo(expected));
+        }
+
+        /* 
+         * Verify that IncreaseStock method works correctly for valid inputs (stock: 123, amount: 456)
+         * - Kuolung Cheng
+         */
+        [Test]
+        public void IncreaseStock_Input123And456_Valid()
+        {
+            // Arrange
+            int stock = 123;
+            int amount = 456;
+            testProduct.StockAmount = stock;
+            int expected = 579;
+
+            // Act
+            testProduct.IncreaseStock(amount);
+            int actual = testProduct.StockAmount;
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        /* 
+         * Verify that IncreaseStock method works correctly for valid inputs (stock: 499999, amount: 1)
+         * - Kuolung Cheng
+         */
+        [Test]
+        public void IncreaseStock_Input499999And1_Valid()
+        {
+            // Arrange
+            int stock = 499999;
+            int amount = 1;
+            testProduct.StockAmount = stock;
+            int expected = 500000;
+
+            // Act
+            testProduct.IncreaseStock(amount);
+            int actual = testProduct.StockAmount;
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        /* 
+         * Verify that calling IncreaseStock method with invalid inputs (stock: 499999, amount: 10)
+         * that increases stock beyond the maximum value (500000) will throw an exception
+         * - Kuolung Cheng
+         */
+        [Test]
+        public void IncreaseStock_Input499999And10_Invalid()
+        {
+            // Arrange
+            int stock = 499999;
+            int amount = 10;
+            testProduct.StockAmount = stock;
+            string expected = "Stock amount must be between 5 and 500000";
+
+            // Act
+            var actual = Assert.Throws<ArgumentException>(() => testProduct.IncreaseStock(amount));
+
+            // Assert
+            Assert.That(actual.Message, Is.EqualTo(expected));
+        }
+    }
+}
